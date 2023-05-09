b3d967b761408fb588e001572d481e51e3f7a586
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-05-09 14:22:58 -0300

    adiciona modal adicionar post

---
+++ public/js/modal_add.js
@@ -0,0 +1,26 @@
+
+var modal3 = document.getElementById("modal-add");
+
+
+
+var btn = document.getElementById("mybtnAdd");
+
+
+var span = document.getElementsByClassName("btnVoltarC")[0];
+
+
+btn.onclick = function() {
+  modal3.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal3.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
