69280d27b6807b1a9e179ef881b66b9cde5dcc0f
danielabib04 <danielabib@outlook.com>
2023-05-09 18:00:29 -0300

    A

--- public/js/modal_excluir.js
+++ public/js/modal_excluir.js
@@ -1,27 +1,19 @@
-
-
 var modal2 = document.getElementById("modal-excluir");
-
-
 
 var btn = document.getElementById("mybtnDel");
 
-
 var span = document.getElementsByClassName("btnVoltarC")[0];
 
-
-btn.onclick = function() {
+btn.onclick = function () {
   modal2.style.display = "block";
-}
+};
 
-
-span.onclick = function() {
+span.onclick = function () {
   modal2.style.display = "none";
-}
+};
 
-
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal2) {
     modal2.style.display = "none";
   }
-}
+};
