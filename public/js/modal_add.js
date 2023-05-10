b0d5d7106dac47b224f1a6115af171bca72bf344
danielabib04 <danielabib@outlook.com>
2023-05-10 19:20:19 -0300

    a

--- public/js/modal_add.js
+++ public/js/modal_add.js
@@ -19,7 +19,7 @@ span.onclick = function() {
 }
 
 
-window3.onclick = function(event) {
+window.onclick = function(event) {
   if (event.target == modal3) {
     modal3.style.display = "none";
   }
