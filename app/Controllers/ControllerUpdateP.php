0f286a15b9809406889bbf194664225ca5d38fb8
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-05-23 14:14:03 -0300

    adiciona funçao update e create no controller

--- app/Controllers/ControllerUpdateP.php
+++ app/Controllers/ControllerUpdateP.php
@@ -13,10 +13,7 @@ class UserController
 
     }
 
-    public function create()
-    {
- 
-    }
+  
 
     public function store()
     {
@@ -28,13 +25,35 @@ class UserController
   
     }
 
-    public function update()
-    {
-        
-    }
-
     public function delete()
     {
  
     }
+
+    public function create()
+    {
+        $parameters = [
+            'title' => $_POST['title'],
+            'content' => $_POST['content'],
+            'image' => $_POST['image'],
+            'created_at' => $_POST['created_at'],
+            'author' => $_POST['author'],
+        ];
+        App::get('radiator_springs')->insert('posts',$parameters);
+        header('location: /admin/lista-de-post-adm.html');
+    } 
+     public function update()
+    {
+        $parameters = [
+            'title' => $_POST['title'],
+            'content' => $_POST['content'],
+            'image' => $_POST['image'],
+            'created_at' => $_POST['created_at'],
+            'author' => $_POST['author'],
+        ];
+        App::get('radiator_springs')->edit($_POST['id'],'posts',$parameters);
+        header('location: /admin/lista-de-post-adm.html');
+    }
 }
+
+?>
--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,7 @@
 <?php
 use App\Controllers\ControllerUpdateP;
-
 use App\Core\Router;
+
+$router ->post('admin/lista-de-post-adm.html',)
 
 ?>
