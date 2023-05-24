a7ddfe09d39a344ddfe77ea8fca46f28fa014e50
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-05-24 14:49:54 -0300

    altera bottao para submit

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,7 @@
 use App\Controllers\ControllerUpdateP;
 use App\Core\Router;
 
-$router ->post('admin', 'ControllerUpdateP@create');
+$router ->post('admin/create', 'ControllerUpdateP@create');
 $router ->post('admin', 'ControllerUpdateP@edit');
 
 ?>
