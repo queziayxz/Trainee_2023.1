ce3de8645797419e7cc1bcb8e4adc8a05942cd71
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-05-24 14:11:47 -0300

    adiciona funçoes do query

--- core/database/QueryBuilder.php
+++ core/database/QueryBuilder.php
@@ -13,4 +13,47 @@ class QueryBuilder
     {
         $this->pdo = $pdo;
     }
+
+    public function insert($table,$parameters)
+    {
+        $sql = sprintf(
+            'INSERT INTO %s (%s) VALUES (%s)',
+            $table , implode(', ', array_keys($parameters)),
+             ':' . implode(', :', array_keys($parameters))
+        );
+
+        try{
+            $stnt = $this ->pdo->prepare($sql);
+            $stnt->execute($parameters);
+
+        } catch (Exception $e)
+        {
+            die($e->getMessage());
+        }
+
+    }
+
+    public function edit($table,$id,$parametros)
+    {
+        $sql = sprintf(
+            'UPDATE %s SET $s WHERE %s',
+            $table ,
+             implode(', ', array_map(function($parametros){
+                return "{$parametros} = :{$parametros}";
+             }, array_keys($parametros))),
+             'id = :id'
+        );
+
+        $parametros['id'] = $id;
+
+        try{
+            $stnt = $this ->pdo->prepare($sql);
+            $stnt->execute($parametros);
+
+        } catch (Exception $e)
+        {
+            die($e->getMessage());
+        }
+
+    }
 }
