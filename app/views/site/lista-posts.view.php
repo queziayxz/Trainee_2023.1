30dbd73258f4a952d33cc35016e88dff813176bc
danielabib04 <danielabib@outlook.com>
2023-05-26 15:12:02 -0300

    src lista de posts

--- app/views/site/lista-posts.view.php
+++ app/views/site/lista-posts.view.php
@@ -28,7 +28,7 @@
             <?php foreach($posts as $post): ?>
                 <div class="miniatura">
 
-                        <img class="link-img" src="../../../public/img/toystory.jpg">
+                        <img class="link-img" src="<?= $post->image?>">
                     <div class="miniatura-text">
                         <h1><?php echo $post->title?></h1>
                         <h2><?php echo substr($post->content, 0, 120) . "...";?></h2>
