296c5d6d090e4bc47517eee89ddd8b11dc154ed3
danielabib04 <danielabib@outlook.com>
2023-05-26 13:56:34 -0300

    nome do posts-lista

--- app/views/site/lista-posts.view.php
+++ app/views/site/lista-posts.view.php
@@ -10,7 +10,6 @@
         <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
         <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
         <meta name="viewport" content="width=device-width, inital-scale=1.0">
-
         <title>Posts - Radiator Springs Gazette</title>
         <link rel="shortcut icon" href="../../../public/assets/logo_blog.jpg">
     </head>
