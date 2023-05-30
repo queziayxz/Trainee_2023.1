a2e85250f802dcdb9a227014ab96e210fa9b2acd
GabrielVieira9768 <gabrieloliveira.vieira@estudante.ufjf.br>
2023-05-30 10:16:14 -0300

    Adiciona NavBar e Footer na lista de posts do usuário

---
+++ app/views/includes/Footer.php
@@ -0,0 +1,26 @@
+<footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h4>Informações de contato</h4>
+          <p>Endereço: Rua Exemplo, 123</p>
+          <p>Telefone: (99) 9999-9999</p>
+          <p>Email: exemplo@exemplo.com</p>
+        </div>
+        <div class="col-md-4">
+          <h4>Links úteis</h4>
+          <ul class="list-unstyled">
+            <li><a href="#">Página inicial</a></li>
+            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <h4>Siga-nos</h4>
+          <ul class="list-unstyled">
+            <li><a href="#"><i class="fab fa-facebook"></i> Facebook</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
---
+++ app/views/includes/NavBar.php
@@ -0,0 +1,27 @@
+<header>
+    <div class="container" id="nav-container">
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <a href="#" class="navbar-brand">
+          <img id="logo" src="/public/assets/logo_blog.jpg" alt="Logo Radiator Springs RS">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <div class="navbar-nav">
+            <ul class="navbar-nav">
+              <li class="nav-item ">
+                <a class="nav-link fw-bold " href="#">Home</a>
+              </li>
+              <li class="nav-item" >
+                <a class="nav-link fw-bold" href="#">Nossos posts</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fw-bold" href="#">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
