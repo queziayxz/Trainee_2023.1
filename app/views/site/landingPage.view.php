8dd90ff2f8e6eb3f9abe84483e547087d2a0e54b
GabrielVieira9768 <gabrieloliveira.vieira@estudante.ufjf.br>
2023-05-31 09:43:54 -0300

    Conserta overflow da Landing Page

--- app/views/site/landingPage.view.php
+++ app/views/site/landingPage.view.php
@@ -20,9 +20,11 @@
   </head>
   <body>
 
-    <?php require('app/views/includes/NavBar.php'); ?>
 
     <main>
+
+      <?php require('app/views/includes/NavBar.php'); ?>
+
       <section id="intro">
         <div id="imgIntro">
           <img src="../../../public/assets/logo2.jpg" alt="imagem principal" />
@@ -94,5 +96,6 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
   crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/31a11ca086.js" crossorigin="anonymous"></script>
 
 </html>
