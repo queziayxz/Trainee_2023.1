9eadf1e5288d731577b737752d0bfc32c198bae6
GabrielVieira9768 <gabrieloliveira.vieira@estudante.ufjf.br>
2023-06-09 14:41:06 -0300

    Altera z-index na página ativa

--- app/routes.php
+++ app/routes.php
@@ -3,16 +3,19 @@
 use App\Controllers\PostController;
 use App\Controllers\UserController;
 
-$router->get('posts/admin', 'PostController@index');
-$router->get('posts', 'PostController@listaPosts');
-$router->post('posts/postIndividual', 'PostController@postIndividual');
-$router->get('home', 'PostController@landingPage');
-$router ->post('posts/create', 'PostController@create');
-$router ->post('posts/edit', 'PostController@edit');
+// Rotas do Admin
+$router->get('usuarios/admin', 'UserController@index'); // Lista de Usuários
+$router->post('usuarios/create', 'UserController@create');
+$router->post('usuarios/update','UserController@update');
+$router->post('usuarios/delete','UserController@delete');
+
+$router->get('posts/admin', 'PostController@index'); // Lista de Posts
+$router->post('posts/create', 'PostController@create');
+$router->post('posts/edit', 'PostController@edit');
 $router->post('posts/delete', 'PostController@delete');
 
-$router->get('usuarios/admin', 'UserController@index');
-$router->post('usuarios/create', 'UserController@create');
-$router->post('usuarios/delete','UserController@delete');
-$router->post('usuarios/update','UserController@update');
+// Rotas do Usuário
+$router->get('home', 'PostController@landingPage');
+$router->get('posts', 'PostController@listaPosts');
+$router->post('posts/postIndividual', 'PostController@postIndividual');
 ?>
